cbc8a2863ff4c4e3dc32b732f673baff81aeefab
Daniel Silva Reis <Pigmoth69@users.noreply.github.com>
2014-10-06 20:00:03 +0000

    falta o display

--- lab2/timer.c
+++ lab2/timer.c
@@ -1,5 +1,8 @@
 #include <minix/syslib.h>
 #include <minix/drivers.h>
+#include "i8254.h"
+#include "timer.h"
+
 
 int timer_set_square(unsigned long timer, unsigned long freq) {
 
@@ -23,18 +26,49 @@ void timer_int_handler() {
 int timer_get_conf(unsigned long timer, unsigned char *st)
 {
 	unsigned long temporario;
-
 	temporario = TIMER_RB_CMD | TIMER_RB_COUNT_  |TIMER_RB_SEL(timer) ;
 	sys_outb(TIMER_CTRL, temporario);
-	sys_inb(TIMER_0+timer, &temporario );
-	*st = (unsigned char)temporario;
+	sys_inb(TIMER_0+timer, (unsigned long *) st );
+	printf("%X",*st);
 	return 0;
 }
 
 int timer_display_conf(unsigned char conf)
 {
+	unsigned char temporario;
+
+	if(BIT(5)|BIT(4) == (BIT(5)|BIT(4))& conf)
+	{
+		printf("Type of access: LSB followed by MSB \n ");
+	}
+	else if(BIT(5) == BIT(5) & conf)
+	{
+		printf("Type of access: MSB \n ");
+	}
+	else
+		printf("Type of access: LSB\n ");
 	
-	return 1;
+	if(BIT(1)  == (BIT(1) | BIT(2) | BIT(3)) & conf)
+		printf("Operating mode: 1\n");
+	else
+	if((BIT(2)  == (BIT(1) | BIT(2) | BIT(3)) & conf)||(BIT(2)|BIT(3)  == (BIT(1) | BIT(2) | BIT(3)) & conf))
+		printf("Operating mode: 2\n");
+	else
+	if(BIT(1) | BIT(2) | BIT(3)  == (BIT(1) | BIT(2) | BIT(3)) & conf|| BIT(1)|BIT(2)  == (BIT(1) | BIT(2) | BIT(3)) & conf)
+			printf("Operating mode: 3\n");
+	else
+	if(BIT(3)  == ((BIT(1) | BIT(2) | BIT(3)) & conf))
+			printf("Operating mode: 4\n");
+	if(BIT(1)|BIT(3)  == ((BIT(1) | BIT(2) | BIT(3)) & conf))
+			printf("Operating mode: 5\n");
+	else
+		printf("Operating mode: 0\n");
+
+	if(BIT(0) == (BIT(0) & conf))
+		printf("Counting mode: BCD (4  decades) \n");
+	else
+		printf("Counting mode: Binary (16 bits) \n");
+	return 0;
 }
 
 int timer_test_square(unsigned long freq) {
@@ -51,7 +85,7 @@ int timer_test_config(unsigned long timer)
 {
 	unsigned char tempo;
 	int erro;
-	if(timer!=0 || timer !=1 ||timer !=2)
+	if(timer<0 || timer >2)
 	{
 		return 1;
 	}
