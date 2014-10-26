cf7ab84ab3699558aa67179006b6c08ae0b45f91
Daniel Silva Reis <Pigmoth69@users.noreply.github.com>
2014-10-26 17:50:54 +0000

    Com a kbd_test_timed_scan

--- lab3/test3.c
+++ lab3/test3.c
@@ -65,10 +65,74 @@ int kbd_test_leds(unsigned short n, unsigned short *leds)
 	return 0;
 }
 
+
+
+
 int kbd_test_timed_scan(unsigned short n)
 {
-    /* To be completed */
+	int counter1=0;
+	int i=0;
+	int ipc_status;
+	int r;
+	message msg;
+	int irq_set1;
+	int irq_set2;
+
+	if(( irq_set1 = KBD_subscribe_int())== -1)
+		return -1;
+	if((irq_set2 = timer_subscribe_int()) == -1)
+		return -1;
+
+
+	while(keyboard != ESC_KEY) {
+		/* Get a request message. */
+		if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0)
+		{
+			printf("driver_receive failed with: %d", r);
+			continue;
+		}
+		if (is_ipc_notify(ipc_status)) { /* received notification */
+			switch (_ENDPOINT_P(msg.m_source)) {
+			case HARDWARE: /* hardware interrupt notification */
+				if (msg.NOTIFY_ARG & irq_set1)
+				{ /* subscribed interrupt */
+
+						KDB_handler_C();
+						i = 0;
+				}
+
+				if(msg.NOTIFY_ARG & irq_set2)
+				{
+					counter1++;
+					if(counter1 % 60 == 0)
+					{
+						i++;
+						//printf("%d",i);
+						//printf("\n");
+					}
+					if(i == n)
+					{
+						keyboard= ESC_KEY;
+						printf("O tempo acabou!\n");
+					}
+
+
+				}
+
+				break;
+			default:
+				break; /* no other notifications expected: do nothing */
+			}
+		} else { /* received a standard message, not a notification */
+			/* no standard messages expected: do nothing */
+		}
+	}
+
+	if(KBD_unsubscribe_int()!= OK)
+		return -1;
+	if(timer_unsubscribe_int()!= OK)
+		return -1;
+
+	return 0;
 }
-
-
 
