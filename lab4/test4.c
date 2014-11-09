58e3021ca3307e55df2f464cd24056afead4554d
up201308586 <up201308586@4f64fe87-4c7d-4307-a8a3-61a7077aca4b>
2014-11-09 15:39:38 +0000

    com algumas coisas

--- lab4/test4.c
+++ lab4/test4.c
@@ -1,5 +1,59 @@
-int test_packet(unsigned short cnt){
-    /* To be completed ... */
+#include"i8042.h"
+
+
+
+int test_packet(unsigned short cnt)
+{
+
+	int x =0;
+	int ipc_status;
+	int r;
+	message msg;
+	int irq_set;
+
+	if(( irq_set = MOUSE_subscribe_int())== -1)
+		return -1;
+
+		while(x < cnt) {
+			/* Get a request message. */
+			if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0)
+			{
+				printf("driver_receive failed with: %d", r);
+				continue;
+			}
+			if (is_ipc_notify(ipc_status)) { /* received notification */
+				switch (_ENDPOINT_P(msg.m_source)) {
+				case HARDWARE: /* hardware interrupt notification */
+					if (msg.NOTIFY_ARG & irq_set)
+					{ /* subscribed interrupt */
+//.............................................................................................................
+						sys_outb(KBDCOMMAND,&MOUSE);
+
+
+
+
+
+
+
+//.............................................................................................................
+					}
+					break;
+				default:
+					break; /* no other notifications expected: do nothing */
+				}
+			} else { /* received a standard message, not a notification */
+				/* no standard messages expected: do nothing */
+			}
+		}
+
+		if(MOUSE_unsubscribe_int()!= OK)
+			return -1;
+		return 0;
+
+
+
+
+
 }	
 	
 int test_async(unsigned short idle_time) {
