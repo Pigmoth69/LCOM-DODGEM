9eecd7240b996056efcb45ae37568b265a661a95
David Baião <joaobaiao12@gmail.com>
2014-11-25 21:27:33 +0000

    com os primeiros ficheiros

---
+++ project/timer.c
@@ -0,0 +1,275 @@
+#include <minix/syslib.h>
+#include <minix/drivers.h>
+#include "timer.h"
+
+int counter = 0;
+int hook_id = 0;
+
+//123
+int getCounter() {return counter;}
+
+int timer_set_square(unsigned long timer, unsigned long freq)
+{
+	unsigned long div = TIMER_FREQ/freq;
+	char conf;
+		if(freq == 0 || freq > TIMER_FREQ || div >= (1 << 16))
+		{
+			return 1;
+		}
+
+		if(timer >= 0 && timer <= 2)
+		{
+
+		if(timer_get_conf(timer,&conf)!= 0)
+		{
+			return 1;
+		}
+
+		if(sys_outb(TIMER_CTRL,TIMER_SEL0 | TIMER_LSB_MSB | TIMER_SQR_WAVE | (conf & BIT(0))) != 0)
+		{
+			return 1;
+		}
+
+		if(sys_outb(TIMER_0+timer, (char)div) != 0)
+		{
+			return 1;
+		}
+
+		if(sys_outb(TIMER_0+timer,(char)(div>>8)) != 0)
+		{
+			return 1;
+		}
+
+		return 0;
+		}
+
+		return 1;
+}
+
+
+
+int timer_subscribe_int(void )
+{
+	int hook;
+	hook = hook_id;
+	if (sys_irqsetpolicy(TIMER0_IRQ,IRQ_REENABLE,&hook_id) == OK)
+		if (sys_irqenable(&hook_id) == OK)
+			return BIT(hook);
+
+	return -1;
+}
+
+int timer_unsubscribe_int()
+{
+	if(sys_irqrmpolicy(&hook_id) == OK)
+		if (sys_irqdisable(&hook_id) == OK)
+			return 0;
+
+	return 1;
+}
+
+void timer_int_handler()
+{
+	counter++;
+}
+
+int timer_get_conf(unsigned long timer, unsigned char *st)
+{
+	unsigned long temporario;
+	int x;
+
+	temporario = TIMER_RB_CMD | TIMER_RB_COUNT_  |TIMER_RB_SEL(timer) ;
+
+	if(sys_outb(TIMER_CTRL, temporario) != 0)
+	{
+		return 1;
+	}
+
+	if(sys_inb(TIMER_0+timer, (unsigned long *) st )!= 0)
+	{
+		return 1;
+	}
+	//printf("%X",*st);
+	//printf("\n");
+	return 0;
+}
+
+int timer_display_conf(unsigned char conf)
+{
+	if(BIT(7) == (BIT(7) & conf))
+	{
+		printf("Output: 1 \n");
+	}
+	else
+	{
+		printf("Output: 0\n");
+	}
+
+
+
+	if((BIT(5)|BIT(4)) == ((BIT(5)|BIT(4))& conf))
+	{
+		printf("Type of access: LSB followed by MSB \n ");
+	}
+	else if(BIT(5) == (BIT(5) & conf))
+	{
+		printf("Type of access: MSB \n ");
+	}
+	else
+		printf("Type of access: LSB\n ");
+	
+	if(((BIT(3)|BIT(2)|BIT(1)) == ((BIT(3)|BIT(2)|BIT(1)) & conf))|| ((BIT(2)|BIT(1)) == ((BIT(2)|BIT(1)) & conf)))
+	{
+		printf("Programmed mode: 3 \n");
+	}
+	else if((BIT(3)|BIT(2))== ((BIT(3)|BIT(2)) & conf))
+	{
+		printf("Programmed mode: 2 \n");
+	}
+	else if((BIT(3)| BIT(1))== ((BIT(3)| BIT(1)) & conf))
+	{
+		printf("Programmed mode: 5 \n");
+	}
+	else if(BIT(3)== (BIT(3) & conf))
+	{
+		printf("Programmed mode: 4 \n");
+	}
+	else if(BIT(2) == (BIT(2) & conf))
+	{
+		printf("Programmed mode: 2 \n");
+	}
+	else if(BIT(1) == (BIT(1) & conf))
+	{
+		printf("Programmed mode: 1 \n");
+	}
+	else
+		printf(" Programmed mode: 0 \n");
+
+
+//mudar o BIT(2) | BIT(1) por BIT_SQ
+	if(BIT(0) == (BIT(0) & conf))
+		printf("Counting mode: BCD (4  decades) \n");
+	else
+		printf("Counting mode: Binary (16 bits) \n");
+	return 0;
+}
+
+
+int timer_test_square(unsigned long freq)
+{
+	return timer_set_square(0,freq);
+}
+
+
+int timer_test_int(unsigned long time)
+{
+	int irq_set = timer_subscribe_int(); //subscreve e inicia as interrupções do timer0
+	timer_set_square(0,60); //coloca a frequencia a 60
+
+	int ipc_status;
+	int r;
+	message msg;
+	unsigned i = 0;
+	while( i < time) { // enquando a contagem é menor que o valor passado no parametro
+		/* Get a request message. */
+		if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0)
+		{
+			printf("driver_receive failed with: %d", r);
+			continue;
+		}
+
+
+
+		if (is_ipc_notify(ipc_status)) { /* received notification */
+			switch (_ENDPOINT_P(msg.m_source)) {
+			 case HARDWARE: /* hardware interrupt notification */
+				 if (msg.NOTIFY_ARG & irq_set)
+				 { /* subscribed interrupt */
+					 timer_int_handler();
+					 if (counter % 60 == 0) //a cada segundo (60 contagens a 60 de frequencia)
+					 {
+						 i++;
+						 //printf("%d", i); // imprime o valor do segundo.
+						// printf("\n");
+					 }
+				 }
+				 break;
+				default:
+					break; /* no other notifications expected: do nothing */
+			}
+		} else { /* received a standard message, not a notification */
+			/* no standard messages expected: do nothing */
+
+			}
+	}
+	if (timer_unsubscribe_int() != OK)  //termina a subscrição, caso dê erro retorna 1
+		return 1;
+	
+	return 0;
+}
+
+int timer_test_config(unsigned long timer)
+{
+	unsigned char tempo;
+	int erro;
+	if(timer<0 || timer >2)
+	{
+		return 1;
+	}
+	erro = timer_get_conf(timer,&tempo);
+
+	if(erro == 1)
+		return 1;
+
+
+	erro = timer_display_conf(tempo);
+
+	if(erro == 1)
+		return 1;
+
+	return 0;
+}
+
+
+void wait_a_second()
+{
+	int irq_set = timer_subscribe_int(); //subscreve e inicia as interrupções do timer0
+	timer_set_square(0,60); //coloca a frequencia a 60
+
+	int ipc_status;
+	int r;
+	message msg;
+	unsigned i = 0;
+	while( i != 1) { // enquando a contagem é menor que o valor passado no parametro
+		/* Get a request message. */
+		if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0)
+		{
+			printf("driver_receive failed with: %d", r);
+			continue;
+		}
+
+
+
+		if (is_ipc_notify(ipc_status)) { /* received notification */
+			switch (_ENDPOINT_P(msg.m_source)) {
+			 case HARDWARE: /* hardware interrupt notification */
+				 if (msg.NOTIFY_ARG & irq_set)
+				 { /* subscribed interrupt */
+					 timer_int_handler();
+					 if (counter % 60 == 0) //a cada segundo (60 contagens a 60 de frequencia)
+					 {
+						 i=1;
+					 }
+				 }
+				 break;
+				default:
+					break; /* no other notifications expected: do nothing */
+			}
+		} else { /* received a standard message, not a notification */
+			/* no standard messages expected: do nothing */
+
+			}
+	}
+	timer_unsubscribe_int();  //termina a subscrição, caso dê erro retorna 1
+}
+
