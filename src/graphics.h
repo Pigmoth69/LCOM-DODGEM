22c5e5cc6b4d2f515a5c331bb1aca5c5dfb30bdb
Daniel Silva Reis <Pigmoth69@users.noreply.github.com>
2014-11-27 21:51:35 +0000

    projeto!

---
+++ src/graphics.h
@@ -0,0 +1,7 @@
+
+
+void graphicsStart(unsigned short mode);
+void graphicsExit();
+void graphics_fill(unsigned long color);
+void set_pixel(unsigned short x, unsigned short y, unsigned long color);
+
---
+++ src/utilities.d
@@ -0,0 +1,2 @@
+
+utilities.o:	utilities.c
