103fed029cb6073467b717245e278f9211c3cdbb
up201308586 <up201308586@4f64fe87-4c7d-4307-a8a3-61a7077aca4b>
2014-12-01 14:40:45 +0000

    cenas

--- DODGEM/src/graphics.h
+++
@@ -1,9 +0,0 @@
-
-
-
-
-void graphicsStart(unsigned short mode);
-void graphicsExit();
-void graphics_fill(unsigned long color);
-void set_pixel(unsigned short x, unsigned short y, unsigned long color);
-short *getVideoBuffer();
--- DODGEM/src/utilities.d
+++
@@ -1,2 +0,0 @@
-
-utilities.o:	utilities.c
