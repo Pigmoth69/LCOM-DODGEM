32e02e7550ade14af3c1992ce9fdda9aeef3159d
up201305195 <up201305195@4f64fe87-4c7d-4307-a8a3-61a7077aca4b>
2014-12-23 17:11:42 +0000

    Os objetos ja mexem

--- DODGEM/src/graphics.h
+++ DODGEM/src/graphics.h
@@ -9,6 +9,7 @@ void graphicsStart(unsigned short mode);
 void graphicsExit();
 void graphics_fill(unsigned long color);
 void set_pixel(unsigned short x, unsigned short y, unsigned long color);
+short *getTripleBuffer();
 short *getVideoBuffer();
 short *getVideoMem();
 void showMainMenu(DODGEM *game);
